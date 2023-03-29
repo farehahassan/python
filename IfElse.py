7799e323ee67f2a4600d230d4e6f67cf58494210
Fareha <hfariha104@gmail.com>
2023-03-30 00:23:52 +0500

    IF elese

---
+++ IfElse.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age"))
+
+if age>= 18 :       #colon for indentation means(after colon give 4 spaces and print anything this will make sure to compiler that this code id for if and for else code go to next line and write code from start of line)
+    print("you are an adult");
+    print("you can vote");
+    
+elif age <18 and age >3 :
+    print("You are in school");
+    
+else:
+    print("You are a child")
+    
+print("Thankyou")
