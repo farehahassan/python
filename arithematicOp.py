02351596b1f07870fa7f2ced669649da63d3b838
Fareha <hfariha104@gmail.com>
2023-03-29 23:39:07 +0500

    Arithematic opertors

---
+++ arithematicOp.py
@@ -0,0 +1,9 @@
+first = int(input("enter first num: "));
+sec = int(input("enter second num: "));
+
+print (first + sec);        #add
+print (first - sec);        #sub
+print (first / sec);        #divide
+print (first // sec);       # double slash use when we dont want values in decimal 
+print (first * sec);        #multiply
+print (first % sec);        #remainder OR modulo
