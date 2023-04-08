0de41d8ee0d5da6083e8ebb87fb709ee11309c0c
Fareha <hfariha104@gmail.com>
2023-04-08 22:55:55 +0500

    Strings

---
+++ Stringg.py
@@ -0,0 +1,9 @@
+# name = input("Enter your name : " )
+# print("Welcome " + name )     #concatinating two strings (place together)
+
+me = "fareha"
+#here f is at index 0 , a is at 1 , r is at 2 , and so on
+print(me[0])
+
+
+
