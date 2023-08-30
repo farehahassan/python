6411ac2fb4d00f2c358b4ed4954f25cfe824c9f8
Fareha <hfariha104@gmail.com>
2023-08-30 09:49:25 +0500

    update

---
+++ forloop.py
@@ -0,0 +1,13 @@
+# colors = ["red" , "yellow" , "orange" , "purple"] 
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+    
+# for k in range(0 , 30 , 2):
+#     print(k)
+i = 0
+while(i<3):
+    print(i)
+    i=i+1
